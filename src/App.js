8cb073dc05b39fccc730af1950a7752ef35c7957
LinhNguyen <nnlinh97it@gmail.com>
2020-07-21 10:01:54 +0700

    moment

--- src/App.js
+++ src/App.js
@@ -11,8 +11,15 @@ import Contact from './components/Contact';
 import Cart from './components/Cart';
 import ProductDetail from './components/ProductDetail';
 import User from './components/User';
+import moment from 'moment';
 
 class App extends Component {
+  componentDidMount() {
+    const date = moment('14-07-1997');
+    console.log('startOfYear', moment().startOf('days'));
+    console.log('startOfMonth', moment().startOf('month').format('DD-MM-YYYY'));
+  }
+
 
   render() {
     return (
